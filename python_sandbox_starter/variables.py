63f69f8266fa1dd64163ba6bcfd64470c0d4250d
Faris Habib <faris.habib@protonmail.ch>
2019-09-17 10:25:42 -0700

    build: add basic data types in python

--- python_sandbox_starter/variables.py
+++ python_sandbox_starter/variables.py
@@ -1,7 +1,7 @@
 # A variable is a container for a value, which can be of various types
 
 '''
-This is a 
+This is a
 multiline comment
 or docstring (used to define a functions purpose)
 can be single or double quotes
@@ -13,3 +13,9 @@ VARIABLE RULES:
   - Must start with a letter or an underscore
   - Can have numbers but can not start with one
 """
+
+# main data types
+x = 1 # int
+y = 2.5 # float point number
+name = 'Bob' # str, can have single or double quotes
+is_cool = True # bool, must be capitalized, otherwise would be looked at as a variable
