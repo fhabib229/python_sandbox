b59a44cb7da2c064799e1ad686b3805a7573ce7f
Faris Habib <faris.habib@protonmail.ch>
2019-09-17 11:57:37 -0700

    build: add string examples

--- python_sandbox_starter/strings.py
+++ python_sandbox_starter/strings.py
@@ -1,5 +1,10 @@
 # Strings in python are surrounded by either single or double quotation marks. Let's look at string formatting and some string methods
 
+name = 'Faris'
+age = 27
+
+# Concatenate
+print('Hello, my name is ' + name + ' and I am ' + str(age)) # would need to cast age because we can't concatenate int
 
 # String Formatting
 
