f04c17ed94ccc2bc74eae562d4a9428be371bd8f
Faris Habib <faris.habib@protonmail.ch>
2019-09-18 13:50:58 -0700

    build: add additional string methods

--- python_sandbox_starter/strings.py
+++ python_sandbox_starter/strings.py
@@ -50,3 +50,31 @@ print(s.swapcase())
 
 # Get length
 print(len(s))
+
+# Replace
+print(s.replace('world', 'everyone'))
+
+# Count
+sub = 'h'
+print(s.count(sub))
+
+# Starts with
+print(s.startswith('hello'))
+
+# Ends with
+print(s.endswith('d'))
+
+# Split into a list
+print(s.split())
+
+# Find position
+print(s.find('r'))
+
+# Is all alphanumeric
+print(s.isalnum())
+
+# Is all alphabetic
+print(s.isalpha())
+
+# Is all numeric
+print(s.isnumeric())
