6fcfbc6d468dbf46b9cb1ddb0455219db2b24b0a
Faris Habib <faris.habib@protonmail.ch>
2019-09-18 13:50:28 -0700

    build: add string methods

--- python_sandbox_starter/strings.py
+++ python_sandbox_starter/strings.py
@@ -19,3 +19,34 @@ age = 27
 # similar to Javascript template literals
 
 # String Methods
+
+s = 'hello world'
+
+# capitalize string
+print(s.capitalize()) # will capitalize the first letter in the string
+
+
+# Arguments by position
+print('My name is {name} and I am {age}'.format(name=name, age=age))
+
+# F-Strings (3.6+)
+print(f'Hello, my name is {name} and I am {age}')
+
+# String Methods
+
+s = 'helloworld'
+
+# Capitalize string
+print(s.capitalize())
+
+# Make all uppercase
+print(s.upper())
+
+# Make all lower
+print(s.lower())
+
+# Swap case
+print(s.swapcase())
+
+# Get length
+print(len(s))
