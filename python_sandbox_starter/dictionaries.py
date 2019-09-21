fc133600bb43b5480be009725fac880e790be2ad
Faris Habib <faris.habib@protonmail.ch>
2019-09-21 10:49:58 -0700

    build: add common dictionary examples

--- python_sandbox_starter/dictionaries.py
+++ python_sandbox_starter/dictionaries.py
@@ -12,7 +12,44 @@ person = {
 # person2 = dict(first_name='Sarah', last_name='Williams')
 
 # Access a single value
-print(person['first_name'])
+# print(person['first_name']) # more common way
+# print(person.get('first_name'))
 
+# Add key/value
+person['phone'] = '123-456-7890'
 
+print(person)
 
+# Get dictionary keys
+print(person.keys())
+
+# get dictionary items
+print(person.items())
+
+# copy dictionary
+
+person2 = person.copy() # similar to spread operator in Javascript
+person2['city'] = 'Seattle'
+print(person2)
+
+# Remove item
+del(person['age'])
+person.pop('phone')
+
+# Clear
+person.clear()
+print(person)
+
+# Get length
+print(len(person2))
+
+# List of dictionaries, like an array of objects in JS
+people = [
+  {'name': 'Martha', 'age': 30},
+  {'name': 'Kevin', 'age': 25}
+]
+
+print(people)
+
+# Getting a specific key/value from a list of dictionaries
+print(people[1]['name'])
