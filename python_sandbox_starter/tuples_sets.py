558fd1897b30fd552ff8be366eb72f0cb4bb6c25
Faris Habib <faris.habib@protonmail.ch>
2019-09-21 10:32:40 -0700

    build: add common tuple methods

--- python_sandbox_starter/tuples_sets.py
+++ python_sandbox_starter/tuples_sets.py
@@ -9,8 +9,27 @@ fruits2 = tuple(('Apples', 'Oranges', 'Grapes'))
 # fruits2 = ('Apples')
 fruits2 = ('Apples', ) # now it's a tuple
 
-print(fruits2, type(fruits2)) # Because there is no trailing comma on line 9, the type is classified as a string
+# print(fruits2, type(fruits2)) # Because there is no trailing comma on line 9, the type is classified as a string
 
+# Retrieving a single value
+print(fruits[1]) # like lists or arrays, tuples are zero-index-based
 
+# Remember, you can't change the values
+# fruits[0] = 'Pears'
+
+# Here's the error : Traceback (most recent call last):
+#   File "tuples_sets.py", line 18, in <module>
+#     fruits[0] = 'Pears'
+# TypeError: 'tuple' object does not support item assignment
+
+# Deleting a tuple
+del fruits2
+# print(fruits2)
+
+# Get length
+len(fruits)
 
 # A Set is a collection which is unordered and unindexed. No duplicate members.
+
+# Create a set
+fruits_set = {'Apples', 'Oranges', 'Mango'}
