1ac3a086c2ecf4c2faf5d09a67da67c916e91ab3
Faris Habib <faris.habib@protonmail.ch>
2019-09-21 10:42:00 -0700

    build: add basic dictionary example

--- python_sandbox_starter/dictionaries.py
+++ python_sandbox_starter/dictionaries.py
@@ -1,1 +1,18 @@
-# A Dictionary is a collection which is unordered, changeable and indexed. No duplicate members.
+# A Dictionary is a collection which is unordered, changeable and indexed. No duplicate members. Very similar to an object literal in JS
+
+# Create a dictionary
+person = {
+  'first_name': 'John', # Keys are in quotes
+  'last_name': 'Doe',
+  'age': 30
+}
+#print(person, type(person))
+
+# Use a constructor
+# person2 = dict(first_name='Sarah', last_name='Williams')
+
+# Access a single value
+print(person['first_name'])
+
+
+
