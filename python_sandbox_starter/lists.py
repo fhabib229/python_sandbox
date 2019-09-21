4b425aab9e23ce0bad70a6621e077af0f7813806
Faris Habib <faris.habib@protonmail.ch>
2019-09-21 10:13:35 -0700

    build: add basic lists

--- python_sandbox_starter/lists.py
+++ python_sandbox_starter/lists.py
@@ -1,1 +1,9 @@
 # A List is a collection which is ordered and changeable. Allows duplicate members.
+
+# Create list
+numbers = [ 1, 2, 3, 4, 5]
+
+# Using a constructor
+numbers2 = list((1,2,3,4,5))
+
+print(numbers, numbers2)
