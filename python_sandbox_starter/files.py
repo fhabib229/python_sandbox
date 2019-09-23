84276a7fac03a72f4a37b32c7af71ff9871bddbb
Faris Habib <faris.habib@protonmail.ch>
2019-09-22 21:00:26 -0700

    build: add basic file examples

--- python_sandbox_starter/files.py
+++ python_sandbox_starter/files.py
@@ -1,1 +1,24 @@
 # Python has functions for creating, reading, updating, and deleting files.
+
+# Open a file
+myFile = open('myfile.txt', 'w') # this will write the file in our starter folder
+
+# Get some info
+print('Name: ', myFile.name) # myFile is an object with properties we can access
+print('Is Closed: ', myFile.closed)
+print('Opening Mode: ', myFile.mode)
+
+# Write to file
+myFile.write('I love Python')
+myFile.write(' and Javascript')
+myFile.close()
+
+# Append to file, now that it is closed in line 14
+myFile = open('myfile.txt', 'a') # a for append
+myFile.write(' I also like PHP')
+myFile.close()
+
+# Read from file
+myFile = open('myfile.txt', 'r+')
+text = myFile.read(100)
+print(text)
