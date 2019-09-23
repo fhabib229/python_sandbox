1624d9930cee7e99597a87b6675da4c1e0f69a81
Faris Habib <faris.habib@protonmail.ch>
2019-09-22 20:16:17 -0700

    build: add simple conditional example

--- python_sandbox_starter/conditionals.py
+++ python_sandbox_starter/conditionals.py
@@ -1,11 +1,19 @@
 # If/ Else conditions are used to decide to do something based on something being true or false
 
-
+x = 10
+y = 50
 
 # Comparison Operators (==, !=, >, <, >=, <=) - Used to compare values
 
+# Simple if
+if x > y:
+  print(f'{x} is greater than {y}')
 
-
+# If/else
+if x > y:
+  print(f'{x} is greater than {y}')
+else:
+  print(f'{y} is greater than {x}')
 # Logical operators (and, or, not) - Used to combine conditional statements
 
 
