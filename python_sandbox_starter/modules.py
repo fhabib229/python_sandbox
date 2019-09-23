05bb2720fdd822b41ea824698beda80e41d55456
Faris Habib <faris.habib@protonmail.ch>
2019-09-22 20:44:54 -0700

    build: add module examples

--- python_sandbox_starter/modules.py
+++ python_sandbox_starter/modules.py
@@ -1,1 +1,40 @@
-# A module is basically a file containing a set of functions to include in your application. There are core python modules, modules you can install using the pip package manager (including Django) as well as custom modules
+# A module is basically a file containing a set of functions to include in your application.
+# There are core python modules, modules you can install using the pip package manager (including Django)
+# as well as custom modules
+
+# core modules
+import datetime
+# today = datetime.date.today() # Lookup the documentation for datetime to see all available properties
+# print(today)
+
+# # Can also just import date
+# from datetime import date
+# today = date.today()
+# print(today)
+
+# importing time
+# import time
+# timestamp = time.time()
+# print(timestamp)
+
+# the example above is not formatted neat, so we can do this instead
+# from time import time
+# timestamp = time()
+# print(timestamp)
+
+# importing the camelcase we just installed
+# pip module
+# from camelcase import CamelCase
+# c = CamelCase()
+# print(c.hump('hello there world'))
+
+# Importing the custom validator module from validator.py
+import validator
+# or could do below
+from validator import validate_email
+
+email = 'test@hello.com'
+if validate_email(email):
+  print('Email is valid')
+else:
+  print('Not a valid Email')
