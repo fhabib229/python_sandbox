96c895f6527d39562b3a2edfee9370328479a0df
Faris Habib <faris.habib@protonmail.ch>
2019-09-22 20:27:33 -0700

    build: add basic loop example

--- python_sandbox_starter/loops.py
+++ python_sandbox_starter/loops.py
@@ -1,5 +1,10 @@
 # A for loop is used for iterating over a sequence (that is either a list, a tuple, a dictionary, a set, or a string).
 
+people = ['John', 'Paul', 'Sarah', 'Susan']
 
+# Simple for loop
+#for in like es6
+for person in people:
+  print(f'Current Person: {person}')
 
 # While loops execute a set of statements as long as a condition is true.
