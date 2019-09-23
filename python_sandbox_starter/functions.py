4c3cc235d50f991699778d3526ab77f205e12247
Faris Habib <faris.habib@protonmail.ch>
2019-09-22 20:10:49 -0700

    build: add additional function example

--- python_sandbox_starter/functions.py
+++ python_sandbox_starter/functions.py
@@ -1,10 +1,16 @@
 # A function is a block of code which only runs when it is called. In Python, we do not use parentheses and curly brackets, we use indentation with tabs or spaces
 
 # Create a function
-def sayHello(name):
+def sayHello(name='Sam'): # we can also set a default like in this line, then we don't have to put a default parameter in line 7
   print(f'Hello {name}')
 
-sayHello('Faris Habib')
+# sayHello()
+def getSum(num1, num2):
+  total = num1 + num2
+  return total
+
+num = getSum(3,4)
+print(num)
 
 # A lambda function is a small anonymous function.
 # A lambda function can take any number of arguments, but can only have one expression. Very similar to JS arrow functions
