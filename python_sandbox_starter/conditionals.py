6040ae385a6d3d63536af169afad86068db99622
Faris Habib <faris.habib@protonmail.ch>
2019-09-22 20:22:15 -0700

    build: add logical operator examples

--- python_sandbox_starter/conditionals.py
+++ python_sandbox_starter/conditionals.py
@@ -1,7 +1,7 @@
 # If/ Else conditions are used to decide to do something based on something being true or false
 
 x = 10
-y = 50
+y = 20
 
 # Comparison Operators (==, !=, >, <, >=, <=) - Used to compare values
 
@@ -10,11 +10,36 @@ if x > y:
   print(f'{x} is greater than {y}')
 
 # If/else
-if x > y:
-  print(f'{x} is greater than {y}')
-else:
-  print(f'{y} is greater than {x}')
+# if x > y:
+#   print(f'{x} is greater than {y}')
+# else:
+#   print(f'{y} is greater than {x}')
+
+# elif
+# if x > y:
+#   print(f'{x} is greater than {y}')
+# elif x == y: #else if
+#   print(f'{x} is equal to {y}')
+# else:
+#   print(f'{y} is greater than {x}')
+
+# Nested if
+# if x > 2:
+#   if x <= 10:
+#     print(f'{x} is greater than 2 and less than or equal to 10') # not a great way to run this conditional, instead use a logical operator like below
 # Logical operators (and, or, not) - Used to combine conditional statements
+
+# like && in JS
+# if x > 2 and x <= 10:
+#   print(f'{x} is greater than 2 and less than or equal to 10')
+
+# # or, Just one has to be true, like || in JS
+# if x > 2 or x <= 10:
+#   print(f'{x} is greater than 2 and less than or equal to 10')
+
+# not
+# if not(x == y):
+#   print(f'{x} is not equal to {y}')
 
 
 
